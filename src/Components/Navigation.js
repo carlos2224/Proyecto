0c6348ad80bbda773d46b6393a2abde7d6c1185a
Carlos <dens.walker22@gmail.com>
2021-03-25 13:47:49 +0100

    first commit

---
+++ src/Components/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+function Navigation() {
+    render(); {
+        return(
+            <nav className="navbar navbnar-dark bg-dark">
+                <a href="" className="text-white">
+                    Tasks 
+                </a>
+            </nav>
+        )
+    }
+}
+export default Navigation;
